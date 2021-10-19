f91c83153ebc849bfe55f08bb013e392a1a9ff3d
Anna W <tester.kodilla@gmail.com>
2021-10-19 22:50:56 +0100

    test parameterisation

--- src/bubble_tea_order_service.test.js
+++ src/bubble_tea_order_service.test.js
@@ -4,39 +4,44 @@ const bubbleTeaMessenger = require('./bubble_tea_messenger');
 jest.mock('./bubble_tea_messenger');
 jest.mock('./simple_logger');
 
-let dummyPaymentDetails;
-
-beforeEach(() => {
-  dummyPaymentDetails = {
+describe('createOrderRequest', () => {
+  // Arrange
+  const dummyPaymentDetails = {
     name: 'Some person',
     address: '123 Some Street',
     debitCard: {
       digits: '123456',
     },
   };
-});
-
-afterEach(() => {
-  jest.clearAllMocks();
-});
-
-test('test successful bubble tea order request', () => {
-  // Arrange
-  const bubbleTeaRequest = {
+  const bubbleTeaRequestMatcha = {
     paymentDetails: dummyPaymentDetails,
     bubbleTea: {
       type: bubbleTeaType.MATCHAMILKTEA,
     },
   };
+  const bubbleTeaRequestLychee = {
+    paymentDetails: dummyPaymentDetails,
+    bubbleTea: {
+      type: bubbleTeaType.LYCHEEICETEA,
+    },
+  };
 
-  // Act
-  const orderRequest = createOrderRequest(bubbleTeaRequest);
+  test.each([
+    [bubbleTeaRequestMatcha, dummyPaymentDetails],
+    [bubbleTeaRequestLychee, dummyPaymentDetails],
+  ])('test %# successful bubble tea order request',
+      (bubbleTeaRequest, dummyPaymentDetails) => {
+        // Act
+        const orderRequest = createOrderRequest(bubbleTeaRequest);
 
-  // Assert
-  expect(orderRequest.name).toBe(dummyPaymentDetails.name);
-  expect(orderRequest.digits).toBe(dummyPaymentDetails.debitCard.digits);
-  expect(bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail)
-      .toHaveBeenCalledWith(orderRequest);
-  expect(bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail)
-      .toHaveBeenCalledTimes(1);
+        // Assert
+        expect(orderRequest.name).toBe(dummyPaymentDetails.name);
+        expect(orderRequest.digits).toBe(dummyPaymentDetails.debitCard.digits);
+        expect(bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail)
+            .toHaveBeenCalledWith(orderRequest);
+        expect(bubbleTeaMessenger.sendBubbleTeaOrderRequestEmail)
+            .toHaveBeenCalledTimes(1);
+
+        jest.clearAllMocks();
+      });
 });
